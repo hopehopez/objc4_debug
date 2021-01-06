b344761562449d5b2778073328ce9a48360f0181
zsq <>
2021-01-06 18:21:02 +0800

    818.2 提交

---
+++ objc4-818.2/ZTest/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  ZTest
+//
+//  Created by zsq on 2021/1/6.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+        
+    }
+    return 0;
+}
